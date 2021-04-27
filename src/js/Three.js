82935d2350731f3a71ef89236868be819268bb8c
prakhar <prakhar0912@gmail.com>
2021-04-27 14:19:25 +0530

    prod resize

--- src/js/Three.js
+++ src/js/Three.js
@@ -1,5 +1,6 @@
 
 import * as THREE from 'three'
+import { Camera } from 'three';
 import CannonHelper from './Utils/CannonHelper'
 
 let initThree = () => {
@@ -8,11 +9,12 @@ let initThree = () => {
     helper = new CannonHelper(scene)
 
     let canva = document.querySelector('.canva')
-    renderer = new THREE.WebGLRenderer({ alpha: true, canvas: canva, antialias: true });
+    renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    document.body.appendChild(renderer.domElement)
     
     // renderer.setPixelRatio(canva.offsetWidth/canva.offsetHeight);
     renderer.setPixelRatio(Math.min(Math.max(window.devicePixelRatio, 1.5), 2))
-    renderer.setSize(canva.offsetWidth, canva.offsetHeight);
+    renderer.setSize( window.innerWidth, window.innerHeight);
     // renderer.shadowMap.enabled = true;
     renderer.physicallyCorrectLights = true
     renderer.gammaFactor = 2.2
@@ -20,7 +22,9 @@ let initThree = () => {
     renderer.autoClear = false
 
     window.addEventListener('resize', () => {
-        renderer.setSize(canva.offsetWidth, canva.offsetHeight)
+        camera.instance.aspect = window.innerWidth/window.innerHeight
+        camera.instance.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
     })
 
     const gridHelper = new THREE.GridHelper(10, 10);
